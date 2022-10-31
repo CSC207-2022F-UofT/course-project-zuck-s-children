130bdcda25604555bf748952a441bae49aa00321
Jiheon lee <jhlee520801@gmail.com>
2022-10-31 12:27:19 -0400

    UI classes and components

--- src/main/java/StudyBuddyApp.java
+++ src/main/java/StudyBuddyApp.java
@@ -1,7 +1,11 @@
-//import UIs
+//import UI
+import UI.UserAuthorizationUI;
 
 public class StudyBuddyApp {
     public static void main(String[] args){
         //initial page: user authorization
+        UserAuthorizationUI userAuthorizationPage = new UserAuthorizationUI();
+
+        userAuthorizationPage.createDialogBox("Example", 540, 300);
     }
 }
