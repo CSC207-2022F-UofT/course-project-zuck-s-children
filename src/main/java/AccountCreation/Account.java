1189a37e6f312a9aa73f3d2a9b766fa3213b95ec
tankenji <kenji.tan@mail.utoronto.ca>
2022-11-16 19:34:45 -0500

    Added Account entity because it didn't last time idk why also edited userdata class but do we want to use that or account entity because they're essentially the same thing. However, I added a static variable arraylist to have all the userdata objects in one place.

---
+++ src/main/java/AccountCreation/Account.java
@@ -0,0 +1,37 @@
+package AccountCreation;
+
+public class Account {
+    private String username;
+    private String password;
+    private Profile profile;
+    private List<Profile> matches;
+    private List<Notification> notifications;
+
+    public Account(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.profile = new Profile;
+        this.matches = new ArrayList<Profile>();
+        this.notifications = new ArrayList<Notification>;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+    public Profile getProfile() {
+        return this.profile;
+    }
+    public List<Profile> getMatches() {
+        return this.matches;
+    }
+
+    public List<Notification> getNotifications() (return this.notifications;)
+
+    public void addMatch() {
+        this.matches.add(Profile);
+    }
+}
--- src/main/java/data/persistency/UserData.java
+++ src/main/java/data/persistency/UserData.java
@@ -1,4 +1,24 @@
 package data.persistency;
 
+import profile.ProfileEntity;
+import AccountCreation.Account;
+
+import java.util.ArrayList;
+
 public class UserData {
+    private String username;
+    private String password;
+    private ProfileEntity profile;
+    private ProfileEntity[] matches;
+    private Notification[] notifications;
+    private static ArrayList<UserData> userDatabase;
+    public UserData(Account userAccount) {
+        this.username = userAccount.getUsername();
+        this.password = userAccount.getPassword();
+        this.profile = userAccount.getProfile();
+        this.matches = userAccount.getMatches();
+        this.notifications = userAccount.getNotifications();
+        userDatabase.add(this);
+    }
+    //TODO: DO WE WANT TO USE USERDATA OR JUST ACCOUNT ENTITIES BECAUSE THEY ARE THE SAME THING ESSENTIALLY?
 }
