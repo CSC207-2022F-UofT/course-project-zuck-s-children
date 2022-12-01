8da34b408f41eeaf04b32ffa78478e5d44832d52
Sanzhar Shamshiyev <102633707+SanzSS@users.noreply.github.com>
2022-12-01 16:50:27 -0500

    Update StudyBuddyApp.java

--- src/main/java/StudyBuddyApp.java
+++ src/main/java/StudyBuddyApp.java
@@ -1,15 +1,14 @@
 //import UI
+import AccountCreation.Account;
 import UI.LoginUI;
-
 import data.persistency.UserDatabase;
-
-
-import javax.swing.*;
 
 //import java.io.FileInputStream;
 //import java.io.FileOutputStream;
 //import java.io.ObjectInputStream;
 //import java.io.ObjectOutputStream;
+
+import javax.swing.*;
 import java.io.*;
 import java.util.HashMap;
 
