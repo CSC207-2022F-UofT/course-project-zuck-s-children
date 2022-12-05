354e0f9bbd618b906c2b3c0cc45a70f73686b8b6
nina <nina.hhhuang@gmail.com>
2022-12-04 23:45:42 -0500

    Added Javadoc to NotificationUI

--- src/main/java/ui/NotificationUI.java
+++ src/main/java/ui/NotificationUI.java
@@ -1,9 +1,6 @@
 package ui;
 
-import notification.clear_notif.*;
 import notification.show_notif.ShowNotifController;
-import notification.show_notif.ShowNotifInteractor;
-import notification.show_notif.ShowNotifPresenter;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -14,6 +11,9 @@ import java.awt.event.ActionListener;
 import static main_app.StudyBuddyApp.*;
 
 public class NotificationUI extends JInternalFrame implements ActionListener{
+    /**
+     * A Notification UI as a tabbed pane in Navigation UI
+     */
     ArrayList<List> notifications;
     public JPanel notifPanel;
     JTable notifTable;
@@ -23,6 +23,10 @@ public class NotificationUI extends JInternalFrame implements ActionListener{
     JScrollPane scrollNotifTable;
     JMenuBar mb;
 
+    /**
+     * Construct a Notification UI Object with a menu bar.
+     * Add action events to buttons.
+     */
     public NotificationUI(){
         super("Notifications");
         setMinimumSize(new Dimension(400, 800));
@@ -32,12 +36,21 @@ public class NotificationUI extends JInternalFrame implements ActionListener{
         this.setLayout(new BorderLayout());
         this.add(mb, BorderLayout.SOUTH);
         this.addActionEvent();
-
     }
 
+    /**
+     * Call on showNotif use case to generate table of all notifications
+     * @param controller Controller for showNotif use case
+     */
     public void create(ShowNotifController controller) {
         controller.showNotif();
     }
+
+    /**
+     * Create JTable based on list of notifications.
+     *
+     * @param notifications Nested List of strings of notifications
+     */
     public JTable makeTable(ArrayList<List> notifications) {
         String[] columnNames = {"Sender", "Content", "Date"};
         String[][] data = getNotif(this.notifications);
@@ -45,6 +58,12 @@ public class NotificationUI extends JInternalFrame implements ActionListener{
         return new JTable(data, columnNames);
     }
 
+    /**
+     * Convert list of Notification objects into nested list of strings
+     * helper method for makeTable().
+     *
+     * @param notifications  Nested List of strings of notifications
+     */
     private String[][] getNotif(ArrayList<List> notifications) {
         int length = notifications.size();
         String[][] data = new String[length][3];
@@ -56,7 +75,12 @@ public class NotificationUI extends JInternalFrame implements ActionListener{
         }
         return data;
     }
-    
+
+    /**
+     * Add table of notifications to the interface
+     *
+     * @param response Nested List of strings of notifications
+     */
     public void prepareView(ArrayList<List> response) {
         this.notifications = response;
         notifTable = makeTable(this.notifications);
@@ -64,16 +88,23 @@ public class NotificationUI extends JInternalFrame implements ActionListener{
         this.add(scrollNotifTable, BorderLayout.CENTER);
     }
 
+    /**
+     * Update interface in responds to clearNotif use case.
+     */
     public void update(){
         JOptionPane.showMessageDialog(this, "No new notifications!");
         this.remove(scrollNotifTable);
         this.repaint();
     }
 
+    /**
+     * Add action events to buttons.
+     */
     public void addActionEvent() {
         chatButton.addActionListener(this);
         clearButton.addActionListener(this);
     }
+
     /**
      * Invoked when an action occurs.
      *
