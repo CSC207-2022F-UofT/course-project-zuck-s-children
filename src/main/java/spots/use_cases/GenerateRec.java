f749d3e22634310036b83c3aff096b9e983f5f84
karenrni <113319323+karenrni@users.noreply.github.com>
2022-12-04 20:18:15 -0500

    Finished Product, except that it is not connected w chatroom

--- src/main/java/spots/use_cases/GenerateRec.java
+++ src/main/java/spots/use_cases/GenerateRec.java
@@ -1,37 +1,39 @@
 package spots.useCases;
 
-
 import AccountCreation.Account;
-import chat.entities.ChatRoomEnt;
+//import chat.entities.ChatRoomEnt;
 import spots.entities.RecGenerator;
 
 import java.util.ArrayList;
-public class GenerateRec implements RecsInBoundary{
+
+public class GenerateRec implements RecsInBoundary {
     private RecsOutBoundary recsPresenter;
 
     Account user1, user2;
     RecGenerator spotRecommender = new RecGenerator();
+
     /**
      * Construct a RecommendedSpots object
+     *
      * @param recsPresenter an OutBoundary
      */
-    public GenerateRec(RecsOutBoundary recsPresenter){ //
+    public GenerateRec(RecsOutBoundary recsPresenter) {
         this.recsPresenter = recsPresenter;
     }
 
-    public void setParticipants(ChatRoomEnt.Participants chatUsers){
-        user1 = chatUsers.User1; // will fix this
-        user2 = chatUsers.User2;
+    public void setParticipants(ArrayList<Account> chatUsers) { //ChatRoomEnt.Participants chatUsers
+        //user1 = chatUsers.User1; // will fix this
+        //user2 = chatUsers.User2;
+        user1 = chatUsers.get(0);
+        user2 = chatUsers.get(1);
     }
 
     /**
      * Generate a recommendation from the users' spot preferences
      * Invoke methods of GenerateRec Object
      */
-    public ArrayList<String> generateRec(){
-//        Account user1 = (Account) users.get(0);
-//        Account user2 = (Account) users.get(1);
-//
+    public ArrayList<String> generateRec() {
+
         ArrayList<String> prefSpots1 = (ArrayList<String>) user1.getProfile().getStudySpotPreferences();
         ArrayList<String> prefSpots2 = (ArrayList<String>) user2.getProfile().getStudySpotPreferences();
 
@@ -39,6 +41,7 @@ public class GenerateRec implements RecsInBoundary{
         spotRecommender.recommender();
         return spotRecommender.getRecommendation();
     }
+
     /**
      * Create a recommendation
      * Update the user interface with the recommendation
